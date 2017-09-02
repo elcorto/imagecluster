d6a939259310380f161d2e46b51b01b64b8f7360
Steve Schmerler <git@elcorto.com>
2017-09-02 22:39:34 +0200

    DOC: move PIL resize doc to play/pil_resample_methods.py

--- calc.py
+++ calc.py
@@ -1,45 +1,6 @@
 # https://github.com/JohannesBuchner/imagehash
 # http://www.hackerfactor.com/blog/index.php?/archives/432-Looks-Like-It.html
 
-# help for PIL.Image.Image.resize
-# -------------------------------
-# 
-# PIL.Image.Image.resize(self, size, resample=0)
-#
-# :param size: The requested size in pixels, as a 2-tuple:
-#    (width, height).
-# :param resample: An optional resampling filter.  This can be
-#    one of :py:attr:`PIL.Image.NEAREST` (use nearest neighbour),
-#    :py:attr:`PIL.Image.BILINEAR` (linear interpolation),
-#    :py:attr:`PIL.Image.BICUBIC` (cubic spline interpolation), or
-#    :py:attr:`PIL.Image.LANCZOS` (a high-quality downsampling filter).
-#    If omitted, or if the image has mode "1" or "P", it is
-#    set :py:attr:`PIL.Image.NEAREST`.
-# :returns: An :py:class:`~PIL.Image.Image` object.
-# 
-# Each PIL.Image.<method> variable is actually an integer (e.g. Image.NEAREST
-# is 0).
-#
-# We tried the resample interpolation methods and measured the speed (ipython's
-# timeit) for resizing an image 3840x2160 -> 8x8. We also compared the resample
-# quality as difference to the best possible resample result, which we defined
-# to be the LANCZOS method (from visual inspection and b/c it is
-# computationally the most elaborate).
-#
-#                                      speed [ms]     
-# Image.NEAREST                   = 0  29.9e-3
-# Image.LANCZOS = Image.ANTIALIAS = 1  123          # reference result
-# Image.BILINEAR                  = 2  47
-# Image.BICUBIC                   = 3  87
-#
-# resample quality (see pil_resample_methods.py)
-# method = 0, diff to ref(1) = 1.0
-# method = 1, diff to ref(1) = 0.0
-# method = 2, diff to ref(1) = 0.135679761399
-# method = 3, diff to ref(1) = 0.0549413095836
-#
-# -> method=2 is probably the best compromise between quality and speed
-
 from imgcmp import env
 import numpy as np
 import scipy.fftpack as fftpack
--- play/pil_resample_methods.py
+++ play/pil_resample_methods.py
@@ -1,3 +1,45 @@
+#!/usr/bin/env python3
+
+# help for PIL.Image.Image.resize
+# -------------------------------
+# 
+# PIL.Image.Image.resize(self, size, resample=0)
+#
+# :param size: The requested size in pixels, as a 2-tuple:
+#    (width, height).
+# :param resample: An optional resampling filter.  This can be
+#    one of :py:attr:`PIL.Image.NEAREST` (use nearest neighbour),
+#    :py:attr:`PIL.Image.BILINEAR` (linear interpolation),
+#    :py:attr:`PIL.Image.BICUBIC` (cubic spline interpolation), or
+#    :py:attr:`PIL.Image.LANCZOS` (a high-quality downsampling filter).
+#    If omitted, or if the image has mode "1" or "P", it is
+#    set :py:attr:`PIL.Image.NEAREST`.
+# :returns: An :py:class:`~PIL.Image.Image` object.
+# 
+# Each PIL.Image.<method> variable is actually an integer (e.g. Image.NEAREST
+# is 0).
+#
+# We tried the resample interpolation methods and measured the speed (ipython's
+# timeit) for resizing an image 3840x2160 -> 8x8. We also compared the resample
+# quality as difference to the best possible resample result, which we defined
+# to be the LANCZOS method (from visual inspection and b/c it is
+# computationally the most elaborate).
+#
+#                                      speed [ms]     
+# Image.NEAREST                   = 0  29.9e-3
+# Image.LANCZOS = Image.ANTIALIAS = 1  123          # reference result
+# Image.BILINEAR                  = 2  47
+# Image.BICUBIC                   = 3  87
+#
+# resample quality (see pil_resample_methods.py)
+# method = 0, diff to ref(1) = 1.0
+# method = 1, diff to ref(1) = 0.0
+# method = 2, diff to ref(1) = 0.135679761399
+# method = 3, diff to ref(1) = 0.0549413095836
+#
+# -> method=2 is probably the best compromise between quality and speed
+
+
 import PIL
 import scipy.misc
 from matplotlib import cm, use
@@ -10,7 +52,7 @@ im = PIL.Image.fromarray(scipy.misc.face())
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-nn = 4
+nn = 16
 img1d = []
 for method in range(4):
     ##arr = np.array(im.convert('L').resize((nn,nn),method).getdata(), dtype=float).reshape(nn,nn)
