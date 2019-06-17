ab5c466005f796c8c757e13ed0f3ed64dd5bfbae
Steve Schmerler <git@elcorto.com>
2019-06-17 07:43:15 +0200

    REL: bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as fd:
 
 setup(
     name='imagecluster',
-    version='0.3.0',
+    version='0.4.0',
     description='cluster images based on image content using a pre-trained ' \
 'deep neural network and hierarchical clustering',
     long_description=long_description,
