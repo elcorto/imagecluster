32d6f3cbcc897f90d8eaed248738e8c8f101bfa9
Steve Schmerler <git@elcorto.com>
2020-10-27 23:36:20 +0100

    MNT: cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,22 @@
-#!/usr/bin/env python3
-
-# publish on pypi
-# ---------------
-#   $ python3 setup.py sdist
-#   $ twine upload dist/imagecluster-x.y.z.tar.gz
-
 import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as fd:
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as fd:
     long_description = fd.read()
 
 
 setup(
-    name='imagecluster',
-    version='0.4.0',
-    description='cluster images based on image content using a pre-trained ' \
-'deep neural network and hierarchical clustering',
+    name="imagecluster",
+    version="0.4.0",
+    description="cluster images based on image content using a pre-trained "
+    "deep neural network and hierarchical clustering",
     long_description=long_description,
-    url='https://github.com/elcorto/imagecluster',
-    author='Steve Schmerler',
-    author_email='git@elcorto.com',
-    license='BSD 3-Clause',
-    keywords='image cluster vgg16 deep-learning',
-    packages=['imagecluster'],
-    install_requires=open('requirements.txt').read().splitlines(),
+    url="https://github.com/elcorto/imagecluster",
+    author="Steve Schmerler",
+    author_email="git@elcorto.com",
+    license="BSD 3-Clause",
+    keywords="image cluster vgg16 deep-learning",
+    packages=["imagecluster"],
+    install_requires=open("requirements.txt").read().splitlines(),
 )
